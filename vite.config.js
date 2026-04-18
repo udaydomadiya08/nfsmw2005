968a363e75d08e9833cb9a14f7e279d2a4f13e42
Uday <uday@Udays-MacBook-Air.local>
2026-04-19 01:30:31 +0530

    Debug: Injected error overlay and refined production paths

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/',
+  base: '',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    emptyOutDir: true
   }
 })
